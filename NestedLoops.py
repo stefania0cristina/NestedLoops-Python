6c146acab60200ee18c5740f26f66c82ba6bc6f9
Stefania Cristina <gheorghestefania320@gmail.com>
2023-09-27 21:41:59 +0100

    Add files via upload

---
+++ NestedLoops.py
@@ -0,0 +1,11 @@
+numbers = [2,2,2,2,5]
+
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
+
+#numbers = [5,2,5,2,2]
+#for x_count in numbers:
+#    print('x' * x_count
